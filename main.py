b168e9aa0ee7d034299324d9b52f740c8ad01821
fridalif <fridalif2@gmail.com>
2025-04-18 09:46:49 +0300

    Запуск grpc сервера

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException
-from services.jwt import verify_token, create_access_token
-from config import get_wireguard_config
-from pydantic import BaseModel
-
-app = FastAPI()
+from handlers.clients.server import ClientHandlerService
+from services.jwt import JWTService
 
 
-class ClientConfigRequest(BaseModel):
-    user_name: str
-    token: str
+def main():
+    jwt_service = JWTService()
+    client_handler_service = ClientHandlerService(jwt_service)
+    client_handler_service.serve()
 
 
-@app.post("/get-config")
-async def get_config(request: ClientConfigRequest):
-    await verify_token(request.token)
-
-    config = await get_wireguard_config(request.user_name)
-
-    return config
-
-
-@app.post("/login")
-async def login(user_name: str):
-    access_token = await create_access_token(data={"user_name": user_name})
-    return {"access_token": access_token, "token_type": "bearer"}
+if __name__ == "__main__":
+    main()
