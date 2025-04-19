fc69c9e4ed7111f3f01587497bd8c12a148b5ff1
fridalif <fridalif2@gmail.com>
2025-04-19 21:06:34 +0300

    Переделан info repsonse в proto файле на типизированный, переделаны фалйы grpc

--- handlers/clients/client_handler_pb2.py
+++ handlers/clients/client_handler_pb2.py
@@ -22,27 +22,28 @@ _runtime_version.ValidateProtobufRuntimeVersion(
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x63lient_handler.proto\x12\x0e\x63lient_handler\"\"\n\nGetRequest\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x01 \x01(\t\"n\n\x0eStatusResponse\x12\"\n\x03\x61\x63k\x18\x01 \x01(\x0b\x32\x13.client_handler.AckH\x00\x12,\n\x04info\x18\x02 \x01(\x0b\x32\x1c.client_handler.InfoResponseH\x00\x42\n\n\x08response\"\x16\n\x03\x41\x63k\x12\x0f\n\x07message\x18\x01 \x01(\t\".\n\x0cInfoResponse\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\x0e\n\x06output\x18\x02 \x01(\t\"\x88\x01\n\x0e\x43onfigResponse\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\x10\n\x06output\x18\x02 \x01(\tH\x00\x12&\n\x05image\x18\x03 \x01(\x0b\x32\x15.client_handler.ImageH\x00\x12\"\n\x03\x61\x63k\x18\x04 \x01(\x0b\x32\x13.client_handler.AckH\x00\x42\x08\n\x06\x63onfig\"\x1b\n\x05Image\x12\x12\n\nimage_data\x18\x01 \x01(\x0c\x32\x80\x02\n\rClientHandler\x12K\n\tGetStatus\x12\x1a.client_handler.GetRequest\x1a\x1e.client_handler.StatusResponse\"\x00\x30\x01\x12R\n\x10GetConnectConfig\x12\x1a.client_handler.GetRequest\x1a\x1e.client_handler.ConfigResponse\"\x00\x30\x01\x12N\n\x0cGetConnectQR\x12\x1a.client_handler.GetRequest\x1a\x1e.client_handler.ConfigResponse\"\x00\x30\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x63lient_handler.proto\x12\x0e\x63lient_handler\x1a\x1fgoogle/protobuf/timestamp.proto\"\"\n\nGetRequest\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x01 \x01(\t\"n\n\x0eStatusResponse\x12\"\n\x03\x61\x63k\x18\x01 \x01(\x0b\x32\x13.client_handler.AckH\x00\x12,\n\x04info\x18\x02 \x01(\x0b\x32\x1c.client_handler.InfoResponseH\x00\x42\n\n\x08response\"\x16\n\x03\x41\x63k\x12\x0f\n\x07message\x18\x01 \x01(\t\"\x8c\x02\n\x0cInfoResponse\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\x0e\n\x06output\x18\x02 \x01(\t\x12\x19\n\x11\x63onnection_status\x18\x03 \x01(\t\x12.\n\ncreated_at\x18\x04 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x11\n\tgigabytes\x18\x05 \x01(\x01\x12\x33\n\x0flast_connection\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x16\n\x0epremium_status\x18\x07 \x01(\x08\x12\x31\n\rpremium_until\x18\x08 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"\x88\x01\n\x0e\x43onfigResponse\x12\x0e\n\x06status\x18\x01 \x01(\x08\x12\x10\n\x06output\x18\x02 \x01(\tH\x00\x12&\n\x05image\x18\x03 \x01(\x0b\x32\x15.client_handler.ImageH\x00\x12\"\n\x03\x61\x63k\x18\x04 \x01(\x0b\x32\x13.client_handler.AckH\x00\x42\x08\n\x06\x63onfig\"\x1b\n\x05Image\x12\x12\n\nimage_data\x18\x01 \x01(\x0c\x32\x80\x02\n\rClientHandler\x12K\n\tGetStatus\x12\x1a.client_handler.GetRequest\x1a\x1e.client_handler.StatusResponse\"\x00\x30\x01\x12R\n\x10GetConnectConfig\x12\x1a.client_handler.GetRequest\x1a\x1e.client_handler.ConfigResponse\"\x00\x30\x01\x12N\n\x0cGetConnectQR\x12\x1a.client_handler.GetRequest\x1a\x1e.client_handler.ConfigResponse\"\x00\x30\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'client_handler_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
-  _globals['_GETREQUEST']._serialized_start=40
-  _globals['_GETREQUEST']._serialized_end=74
-  _globals['_STATUSRESPONSE']._serialized_start=76
-  _globals['_STATUSRESPONSE']._serialized_end=186
-  _globals['_ACK']._serialized_start=188
-  _globals['_ACK']._serialized_end=210
-  _globals['_INFORESPONSE']._serialized_start=212
-  _globals['_INFORESPONSE']._serialized_end=258
-  _globals['_CONFIGRESPONSE']._serialized_start=261
-  _globals['_CONFIGRESPONSE']._serialized_end=397
-  _globals['_IMAGE']._serialized_start=399
-  _globals['_IMAGE']._serialized_end=426
-  _globals['_CLIENTHANDLER']._serialized_start=429
-  _globals['_CLIENTHANDLER']._serialized_end=685
+  _globals['_GETREQUEST']._serialized_start=73
+  _globals['_GETREQUEST']._serialized_end=107
+  _globals['_STATUSRESPONSE']._serialized_start=109
+  _globals['_STATUSRESPONSE']._serialized_end=219
+  _globals['_ACK']._serialized_start=221
+  _globals['_ACK']._serialized_end=243
+  _globals['_INFORESPONSE']._serialized_start=246
+  _globals['_INFORESPONSE']._serialized_end=514
+  _globals['_CONFIGRESPONSE']._serialized_start=517
+  _globals['_CONFIGRESPONSE']._serialized_end=653
+  _globals['_IMAGE']._serialized_start=655
+  _globals['_IMAGE']._serialized_end=682
+  _globals['_CLIENTHANDLER']._serialized_start=685
+  _globals['_CLIENTHANDLER']._serialized_end=941
 # @@protoc_insertion_point(module_scope)
